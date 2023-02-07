32c5dd96af62bc71068ea932f8035b7f909c9b55
IglanCardeal <cmtcardeal@outlook.com>
2023-02-07 13:58:36 -0300

    feat: add validation for service request controller

--- src/infra/app.controller.ts
+++ src/infra/app.controller.ts
@@ -1,9 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common'
+import { ControllerCreateRequestServiceDTO } from './dto/create-request-service-dto'
 
 @Controller('api/v1/service-request')
 export class AppController {
   @Post('create')
-  create(@Body() _request: any) {
+  create(@Body() request: ControllerCreateRequestServiceDTO) {
+    // eslint-disable-next-line no-console
+    console.dir(request.deliveries, { depth: Infinity })
     return {
       message: 'Service Created',
     }
