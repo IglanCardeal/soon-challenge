3ab390b86efdf7011912d60cdce978f562551851
IglanCardeal <cmtcardeal@outlook.com>
2023-02-07 16:10:07 -0300

    test: ensure PostgreServiceRequestRepository create a service request on success

---
+++ src/infra/repository/service-request/faker.ts
@@ -0,0 +1,81 @@
+import { ServiceRequest } from 'src/domain/model/service-request'
+import { createRandomCompanyClient } from '../utils'
+
+const makeFakeServiceRequest = async (): Promise<
+  Omit<ServiceRequest, 'id'>
+> => ({
+  collectionAddress: {
+    lat: -58.6521,
+    long: -87.1162,
+  },
+  company: await createRandomCompanyClient('test', 1),
+  serviceType: 'guincho',
+  createdAt: new Date(),
+  total: {
+    distance: 5,
+    duration: 4,
+    servicePrice: 25.5,
+  },
+  vehicles: [
+    {
+      brand: 'VW',
+      model: 'Gol',
+      year: '2015',
+      plate: 'ABC-1234',
+    },
+    {
+      brand: 'Ford',
+      model: 'Wrangler',
+      year: '2012',
+      plate: 'CDE-1234',
+    },
+  ],
+  deliveries: [
+    {
+      finalAddress: {
+        lat: -58.7523,
+        long: -87.2181,
+      },
+      lastAddress: {
+        lat: -58.6521,
+        long: -87.1162,
+      },
+      total: {
+        distance: 2,
+        duration: 2,
+      },
+      vehicles: [
+        {
+          brand: 'Ford',
+          model: 'Wrangler',
+          year: '2012',
+          plate: 'CDE-1234',
+        },
+      ],
+    },
+    {
+      finalAddress: {
+        lat: -58.7521,
+        long: -87.2162,
+      },
+      lastAddress: {
+        lat: -58.7523,
+        long: -87.2181,
+      },
+      total: {
+        distance: 3,
+        duration: 2,
+      },
+      vehicles: [
+        {
+          brand: 'VW',
+          model: 'Gol',
+          year: '2015',
+          plate: 'ABC-1234',
+        },
+      ],
+    },
+  ],
+})
+
+export { makeFakeServiceRequest }
