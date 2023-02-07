aee3bc6bfc86d8094e96d0b5a638ab05eff2622c
IglanCardeal <cmtcardeal@outlook.com>
2023-02-07 19:27:16 -0300

    test: ensure FindServiceRequestUseCase calls FindServiceRequestRepository with correct service id

---
+++ src/usecases/find-service-request/find-service-request-usecase.spec.ts
@@ -0,0 +1,32 @@
+import {
+  FindServiceRequestRepository,
+  ServiceRequest,
+} from './find-service-request-contracts'
+import { FindServiceRequestUseCase } from './find-service-request-usecase'
+
+class FindServiceRequestRepositoryStub implements FindServiceRequestRepository {
+  async findById(id: string): Promise<ServiceRequest> {
+    return {
+      id,
+    } as ServiceRequest
+  }
+}
+
+const makeSut = () => {
+  const findServiceRequestRepositoryStub =
+    new FindServiceRequestRepositoryStub()
+  return {
+    sut: new FindServiceRequestUseCase(findServiceRequestRepositoryStub),
+    findServiceRequestRepositoryStub,
+  }
+}
+
+describe('FindServiceRequestUseCase', () => {
+  const { sut, findServiceRequestRepositoryStub } = makeSut()
+
+  it('Should call FindServiceRequestRepository with correct service id', async () => {
+    const findByIdSpy = jest.spyOn(findServiceRequestRepositoryStub, 'findById')
+    await sut.find('A123')
+    expect(findByIdSpy).toHaveBeenCalledWith('A123')
+  })
+})
