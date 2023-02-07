5a38fec96e6baa1e773eea7dd890b2b1fec86e6a
IglanCardeal <cmtcardeal@outlook.com>
2023-02-07 01:29:40 -0300

    test: ensure CreateServiceRequestUseCase calls ServiceRequestRepository with correct formated and ordered values

--- src/usecases/service-request/create.spec.ts
+++ src/usecases/service-request/create.spec.ts
@@ -6,15 +6,32 @@ import {
 import { CreateServiceRequestDTO } from 'src/domain/usecases/service-request/create-dto'
 import { DomainConstants } from 'src/domain/constants'
 import { CreateServiceRequestUseCase } from './create'
-import { CalculateDistanceProvider } from 'src/domain/contracts'
-import { Address } from 'src/domain/model/service-request'
+import {
+  CalculateDistanceAndDurationProvider,
+  CalculateDistanceAndDurationProviderResponse,
+  ServiceRequestRepository,
+} from 'src/domain/contracts'
+import { Address, ServiceRequest } from 'src/domain/model/service-request'
 
-class CalculateDistanceProviderStub implements CalculateDistanceProvider {
+class CalculateDistanceAndDurationProviderStub
+  implements CalculateDistanceAndDurationProvider
+{
   async fromOriginToDestiny(
     _origin: Address,
     _destiny: Address,
-  ): Promise<number> {
-    return 0
+  ): Promise<CalculateDistanceAndDurationProviderResponse> {
+    return {
+      distance: 1,
+      duration: 1,
+    }
+  }
+}
+
+class ServiceRequestRepositoryStub implements ServiceRequestRepository {
+  async save(
+    _serviceRequest: Omit<ServiceRequest, 'id'>,
+  ): Promise<ServiceRequest> {
+    return {} as ServiceRequest
   }
 }
 
@@ -24,17 +41,33 @@ const makeFakeDomainConstants = () => ({
       guincho: 2,
       cegonha: 11,
     },
+    pricePerKm: {
+      guincho: {
+        maxGuinchoKmForBasePrice: 4,
+        km: 5,
+        extraKm: 5 + 0.5,
+      },
+      cegonha: {
+        maxCegonhaKmForBasePrice: 4,
+        km: 10,
+        extraKm: 10 + 1.5,
+      },
+    },
     validServiceTypes: ['guincho', 'cegonha'],
   },
 })
 const makeSut = () => {
-  const calculateDistanceProviderStub = new CalculateDistanceProviderStub()
+  const calculateDistanceAndDurationStub =
+    new CalculateDistanceAndDurationProviderStub()
+  const serviceRequestRepositoryStub = new ServiceRequestRepositoryStub()
   return {
     sut: new CreateServiceRequestUseCase(
       makeFakeDomainConstants() as DomainConstants,
-      calculateDistanceProviderStub,
+      calculateDistanceAndDurationStub,
+      serviceRequestRepositoryStub,
     ),
-    calculateDistanceProviderStub,
+    calculateDistanceAndDurationStub,
+    serviceRequestRepositoryStub,
   }
 }
 const makeFakeDto = (): CreateServiceRequestDTO => ({
@@ -81,7 +114,11 @@ const makeFakeDto = (): CreateServiceRequestDTO => ({
 
 describe('CreateServiceRequestUseCase', () => {
   let fakeDto: CreateServiceRequestDTO
-  const { sut, calculateDistanceProviderStub } = makeSut()
+  const {
+    sut,
+    calculateDistanceAndDurationStub,
+    serviceRequestRepositoryStub,
+  } = makeSut()
 
   beforeEach(() => {
     fakeDto = makeFakeDto()
@@ -160,11 +197,10 @@ describe('CreateServiceRequestUseCase', () => {
 
   it('Should call CalculateDistanceProvider with correct values', async () => {
     const fromOriginToDestinySpy = jest.spyOn(
-      calculateDistanceProviderStub,
+      calculateDistanceAndDurationStub,
       'fromOriginToDestiny',
     )
     await sut.create(fakeDto)
-    expect(fromOriginToDestinySpy).toBeCalledTimes(2)
     expect(fromOriginToDestinySpy).toHaveBeenNthCalledWith(
       1,
       { lat: -58.6521, long: -87.1162 },
@@ -175,5 +211,110 @@ describe('CreateServiceRequestUseCase', () => {
       { lat: -58.6521, long: -87.1162 },
       { lat: -58.7523, long: -87.2181 },
     )
+  })
+
+  it('Should call serviceRequestRepository with correct formated and ordered values', async () => {
+    const expectedServiceRequest = {
+      collectionAddress: {
+        lat: -58.6521,
+        long: -87.1162,
+      },
+      company: {
+        id: 'company_id',
+        name: 'Wells',
+      },
+      serviceType: 'guincho',
+      createdAt: expect.any(Date),
+      total: {
+        distance: 5,
+        duration: 4,
+        servicePrice: 25.5,
+      },
+      vehicles: [
+        {
+          brand: 'VW',
+          model: 'Gol',
+          year: '2015',
+          plate: 'ABC-1234',
+        },
+        {
+          brand: 'Ford',
+          model: 'Wrangler',
+          year: '2012',
+          plate: 'CDE-1234',
+        },
+      ],
+      deliveries: [
+        {
+          finalAddress: {
+            lat: -58.7523,
+            long: -87.2181,
+          },
+          lastAddress: {
+            lat: -58.6521,
+            long: -87.1162,
+          },
+          total: {
+            distance: 2,
+            duration: 2,
+          },
+          vehicles: [
+            {
+              brand: 'Ford',
+              model: 'Wrangler',
+              year: '2012',
+              plate: 'CDE-1234',
+            },
+          ],
+        },
+        {
+          finalAddress: {
+            lat: -58.7521,
+            long: -87.2162,
+          },
+          lastAddress: {
+            lat: -58.7523,
+            long: -87.2181,
+          },
+          total: {
+            distance: 3,
+            duration: 2,
+          },
+          vehicles: [
+            {
+              brand: 'VW',
+              model: 'Gol',
+              year: '2015',
+              plate: 'ABC-1234',
+            },
+          ],
+        },
+      ],
+    }
+    const fromOriginToDestinySpy = jest.spyOn(
+      calculateDistanceAndDurationStub,
+      'fromOriginToDestiny',
+    )
+    fromOriginToDestinySpy
+      .mockResolvedValueOnce({
+        distance: 3,
+        duration: 3,
+      })
+      .mockResolvedValueOnce({
+        distance: 2,
+        duration: 1,
+      })
+      .mockResolvedValueOnce({
+        distance: 2,
+        duration: 2,
+      })
+      .mockResolvedValueOnce({
+        distance: 3,
+        duration: 2,
+      })
+    const saveSpy = jest.spyOn(serviceRequestRepositoryStub, 'save')
+    await sut.create(fakeDto)
+    expect(saveSpy).toHaveBeenCalled()
+    expect(saveSpy).toHaveBeenCalledWith(expectedServiceRequest)
   })
 })
