dd37bda2b7e9377e55436ec18477111a59d3acc2
IglanCardeal <cmtcardeal@outlook.com>
2023-02-07 23:54:14 -0300

    test: return company services filtered by date

--- src/infra/repository/postgres/test/service-request-repository.spec.ts
+++ src/infra/repository/postgres/test/service-request-repository.spec.ts
@@ -22,4 +22,34 @@ describe('PostgreServiceRequestRepository', () => {
     const result = await sut.findById(service.id)
     expect(result).toEqual(findExpectation)
   })
+
+  it('Should return all services by company id and filtered by date on success', async () => {
+    const startDate = new Date()
+    const endDate = new Date()
+    startDate.setUTCHours(0, 0, 0, 0)
+    endDate.setUTCHours(23, 59, 59, 999)
+    const payload = {
+      companyId: 1,
+      startDate,
+      endDate,
+    }
+    await sut.save(await makeFakeServiceRequest())
+    const result = await sut.findByCompanyId(payload)
+    expect(result.length).toBe(1)
+  })
+
+  it('Should return an empty result for the dates', async () => {
+    const startDate = new Date()
+    const endDate = new Date()
+    startDate.setMonth(startDate.getMonth() + 1)
+    endDate.setFullYear(endDate.getFullYear() + 1)
+    const payload = {
+      companyId: 1,
+      startDate,
+      endDate,
+    }
+    await sut.save(await makeFakeServiceRequest())
+    const result = await sut.findByCompanyId(payload)
+    expect(result.length).toBe(0)
+  })
 })
